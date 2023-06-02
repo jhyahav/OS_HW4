182d8935a953aee3f2e43e1b8cd4f1ed9050283b
Jonathan Yahav <jonathanhuck@mail.tau.ac.il>
2023-06-02 12:25:31 +0300

    started working on enqueue, added some todos

--- queue.c
+++ queue.c
@@ -1,11 +1,12 @@
 #include "queue.h"
+#include <threads.h>
 
 // We implement the queue as a linked list, saving its head and tail.
 struct Queue
 {
     struct Element *head;
     struct Element *tail;
-    size_t queue_size;
+    size_t queue_size; // TODO: make me atomic?
     size_t waiting_count;
     size_t visited_count;
 };
@@ -18,6 +19,8 @@ struct Element
 
 static struct Queue queue;
 
+struct Element *create_element(const void *data);
+
 void initQueue(void)
 {
 }
@@ -28,6 +31,37 @@ void destroyQueue(void)
 
 void enqueue(const void *element_data)
 {
+    struct Element *new_element = create_element(element_data);
+    // TODO: add locking around next line
+    add_element_to_queue(new_element);
+}
+
+struct Element *create_element(const void *data)
+{
+    // We assume malloc does not fail, as per the instructions.
+    struct Element *element = (struct Element *)malloc(sizeof(struct Element));
+    element->data = data;
+    element->next = NULL;
+    return element;
+}
+
+void add_element_to_queue(struct Element *new_element)
+{
+    queue.queue_size == 0 ? add_element_to_empty_queue(new_element) : add_element_to_nonempty_queue(new_element);
+}
+
+void add_element_to_empty_queue(struct Element *new_element)
+{
+    queue.head = new_element;
+    queue.tail = new_element;
+    queue.queue_size += 1;
+}
+
+void add_element_to_nonempty_queue(struct Element *new_element)
+{
+    queue.tail->next = new_element;
+    queue.tail = new_element;
+    queue.queue_size += 1;
 }
 
 void *dequeue(void)
@@ -45,6 +79,7 @@ size_t size(void)
 
 size_t waiting(void)
 {
+    // TODO: add locking here(?)
     return queue.waiting_count;
 }
 
